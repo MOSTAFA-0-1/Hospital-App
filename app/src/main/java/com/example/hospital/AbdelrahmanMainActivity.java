7f52c8bec118d24329269fa0d595c61d000225e6
AbdelrahmanTechCS <WebDevelopmentLJ@gmail.com>
2022-11-30 20:23:08 +0200

    Commit Abdelrahman Ramadan's files

---
+++ app/src/main/java/com/example/hospital/AbdelrahmanMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.hospital;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AbdelrahmanMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abdelrahman_activity_main);
+    }
+}
