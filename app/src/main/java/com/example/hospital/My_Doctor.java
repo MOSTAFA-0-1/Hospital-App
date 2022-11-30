6f011553d619d064e9a7e369a677373c1a0711d4
mohamedhassanabdullah <mh5418067@gmail.com>
2022-11-30 19:43:41 +0200

    add My Doctor Screen

---
+++ app/src/main/java/com/example/hospital/My_Doctor.java
@@ -0,0 +1,14 @@
+package com.example.hospital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class My_Doctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_doctor);
+    }
+}
