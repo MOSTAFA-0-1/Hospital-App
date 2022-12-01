bc76a42da81c14f30dfdf77249133a41019a4159
Admin <assemstudy86@gmail.com>
2022-12-01 02:25:08 +0200

    sign up doctor_details popular_doctors

--- app/src/main/java/com/example/hospital/sign_up.java
+++ app/src/main/java/com/example/hospital/sign_up.java
@@ -3,15 +3,12 @@ package com.example.hospital;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
-public class doctor_details extends AppCompatActivity {
-
+public class sign_up extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
     }
 }
-
