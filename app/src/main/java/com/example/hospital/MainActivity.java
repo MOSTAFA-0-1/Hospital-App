2fbce18e3b53d57b7e4a65352242f48fa531e665
hospital_project <mostafaabdalalim23@gmail.com>
2022-12-01 02:43:59 +0200

    success upload

--- app/src/main/java/com/example/hospital/MainActivity.java
+++ app/src/main/java/com/example/hospital/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
 //        Map<String, Object> user = new HashMap<>();
 //        user.put("first", "Ada");
 //        user.put("last", "Lovelace");
