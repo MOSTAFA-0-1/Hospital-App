29fa10da57587efb53bf41fe79b2a2abfad531de
hospital_project <mostafaabdalalim23@gmail.com>
2022-12-01 02:24:45 +0200

    success upload

--- app/src/main/java/com/example/hospital/MainActivity.java
+++ app/src/main/java/com/example/hospital/MainActivity.java
@@ -18,55 +18,55 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "";
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    ImageView imageView;
-    Boolean isFavourite = false;
-    FirebaseAuth auth = FirebaseAuth.getInstance();
-    String emial = "";
-    String password = "";
+//    private static final String TAG = "";
+//    FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    ImageView imageView;
+//    Boolean isFavourite = false;
+//    FirebaseAuth auth = FirebaseAuth.getInstance();
+//    String emial = "";
+//    String password = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-        auth.createUserWithEmailAndPassword(emial,password).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-
-            }
-        });
-        auth.signInWithEmailAndPassword(emial,password);
-
-//        db.collection("users")
-//                .add(user)
-//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        setContentView(R.layout.activity_log_in);
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first", "Ada");
+//        user.put("last", "Lovelace");
+//        user.put("born", 1815);
+//        auth.createUserWithEmailAndPassword(emial,password).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
 //
-//                    public void onSuccess(DocumentReference documentReference) {
-//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
+//            }
+//        });
+//        auth.signInWithEmailAndPassword(emial,password);
 //
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.w(TAG, "Error adding document", e);
-//                    }
-//                });
-
-           imageView = findViewById(R.id.favourite);
-        imageView.setColorFilter(getResources().getColor(R.color.white));
-        imageView.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View view) {
-                   isFavourite = !isFavourite;
-                   if (isFavourite)
-                   imageView.setColorFilter(getResources().getColor(R.color.primary));
-                   else
-                   imageView.setColorFilter(getResources().getColor(R.color.white));
-               }
-           });
+////        db.collection("users")
+////                .add(user)
+////                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+////
+////                    public void onSuccess(DocumentReference documentReference) {
+////                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+////                    }
+////                })
+////                .addOnFailureListener(new OnFailureListener() {
+////
+////                    public void onFailure(@NonNull Exception e) {
+////                        Log.w(TAG, "Error adding document", e);
+////                    }
+////                });
+//
+//           imageView = findViewById(R.id.favourite);
+//        imageView.setColorFilter(getResources().getColor(R.color.white));
+//        imageView.setOnClickListener(new View.OnClickListener() {
+//               @Override
+//               public void onClick(View view) {
+//                   isFavourite = !isFavourite;
+//                   if (isFavourite)
+//                   imageView.setColorFilter(getResources().getColor(R.color.primary));
+//                   else
+//                   imageView.setColorFilter(getResources().getColor(R.color.white));
+//               }
+//           });
     }
 }
