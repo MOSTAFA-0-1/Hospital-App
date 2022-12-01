7dc6f19bf878710abd2d56c1a58ac0e237f80e8d
Admin <assemstudy86@gmail.com>
2022-12-01 03:26:55 +0200

    sign up doctor_details popular_doctors

--- app/src/main/java/com/example/hospital/LogIn.java
+++ app/src/main/java/com/example/hospital/LogIn.java
@@ -2,13 +2,24 @@ package com.example.hospital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LogIn extends AppCompatActivity {
-
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        textView = findViewById(R.id.textView4);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),sign_up.class);
+                startActivity(intent);
+            }
+        });
     }
 }
