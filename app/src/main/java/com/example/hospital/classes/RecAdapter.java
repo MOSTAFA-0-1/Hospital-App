5716a3498c338fd58f86446c1ee5ad119ebe22da
hospital_project <mostafaabdalalim23@gmail.com>
2022-12-08 23:24:56 +0200

    test screen done

--- app/src/main/java/com/example/hospital/classes/RecAdapter.java
+++ app/src/main/java/com/example/hospital/classes/RecAdapter.java
@@ -27,13 +27,13 @@ public class RecAdapter extends RecyclerView.Adapter<RecAdapter.ViewHolder>{
          * (custom ViewHolder).
          */
         public static class ViewHolder extends RecyclerView.ViewHolder {
-            private final TextView textView;
+              TextView textView;
             ImageView imageView;
             public ViewHolder(View view) {
                 super(view);
                 // Define click listener for the ViewHolder's View
-                imageView = view.findViewById(R.id.imagetest);
-                textView = (TextView) view.findViewById(R.id.testtext);
+                imageView = view.findViewById(R.id.testsdoc);
+//                textView = (TextView) view.findViewById(R.id.testtext);
             }
 
             public TextView getTextView() {
@@ -52,7 +52,7 @@ public class RecAdapter extends RecyclerView.Adapter<RecAdapter.ViewHolder>{
         public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
             // Create a new view, which defines the UI of the list item
             View view = LayoutInflater.from(context)
-                    .inflate(R.layout.testlistitem, viewGroup, false);
+                    .inflate(R.layout.tests_single_item, viewGroup, false);
 
             return new ViewHolder(view);
         }
@@ -63,14 +63,14 @@ public class RecAdapter extends RecyclerView.Adapter<RecAdapter.ViewHolder>{
         @Override
         public void onBindViewHolder(ViewHolder viewHolder, final int position) {
 
-            viewHolder.getTextView().setText(provider.doctors.get(position).getFirstName() + provider.doctors.get(position).getLastName());
+//            viewHolder.getTextView().setText(provider.doctors.get(position).getFirstName() + provider.doctors.get(position).getLastName());
             viewHolder.imageView.setImageResource(images[position]);
         }
 
         // Return the size of your dataset (invoked by the layout manager)
         @Override
         public int getItemCount() {
-            return provider.doctors.size();
+            return images.length;
         }
     }
 
--- app/src/main/java/com/example/hospital/home.java
+++ app/src/main/java/com/example/hospital/home.java
@@ -1,19 +1,24 @@
 package com.example.hospital;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class home extends AppCompatActivity {
     Button go;
@@ -24,10 +29,24 @@ public class home extends AppCompatActivity {
     ActionBarDrawerToggle toggle;
     TextView name;
     ImageView imageView;
+    MenuItem item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+//        item = findViewById(R.id.logout);
+//        item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem menuItem) {
+////                FirebaseAuth.getInstance().signOut();
+////                Intent i = new Intent(home.this,sign_up.class);
+////                startActivity(i);
+//               return true;
+//            }
+//        });
+
 
         go =(Button) findViewById(R.id.button7);
         go.setOnClickListener(new View.OnClickListener() {
@@ -62,5 +81,34 @@ public class home extends AppCompatActivity {
              imageView = findViewById(R.id.imageurl);
              imageView.setImageResource(drawableId);
 
+    }
+
+    @Override
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.drawer_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                Intent i = new Intent(home.this,sign_up.class);
+                startActivity(i);
+                return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
