8cd77245ed8ae19db7c2079a338a3238d364a53c
Abd-El-Rahman-Mohamed <90394194+Abd-El-Rahman-Mohamed@users.noreply.github.com>
2022-12-10 19:15:57 +0200

    Creating the Adapter for Abdelrahman Ramadan's files ("AllRecAdapter")

---
+++ app/src/main/java/com/example/hospital/classes/AllRecAdapter.java
@@ -0,0 +1,66 @@
+package com.example.hospital.classes;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hospital.R;
+import com.example.hospital.provider;
+
+import java.security.Provider;
+
+public class AllRecAdapter extends RecyclerView.Adapter<AllRecAdapter.ViewHolder>{
+
+    int[] images;
+    Context context;
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+        ImageView imageView;
+        public ViewHolder(View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View
+            imageView = view.findViewById(R.id.record);
+        }
+
+        public TextView getTextView() { return textView; }
+    }
+
+    public AllRecAdapter (Context context,int[] images) {
+        this.context = context ;
+        this.images = images;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(context)
+                .inflate(R.layout.activity_all_records, viewGroup, false);
+
+        return new ViewHolder(view);
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+
+//            viewHolder.getTextView().setText(provider.doctors.get(position).getFirstName() + provider.doctors.get(position).getLastName());
+        viewHolder.imageView.setImageResource(images[position]);
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+}
