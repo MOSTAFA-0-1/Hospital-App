8ef062ee3ea00bb50d3a9a9e034b1cb8e4551b5f
hospital_project <mostafaabdalalim23@gmail.com>
2022-12-17 17:01:27 +0200

    intent

---
+++ app/src/main/java/com/example/hospital/classes/User.java
@@ -0,0 +1,41 @@
+package com.example.hospital.classes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    String Name,status,number,birthDay;
+    public List<Doctor> favouriteDoctors = new ArrayList<Doctor>();
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(String birthDay) {
+        this.birthDay = birthDay;
+    }
+}
