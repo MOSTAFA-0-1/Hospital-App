ec746c12536ee9e2cca38fcc1bb151ac066da708
mohamedhassanabdullah <mh5418067@gmail.com>
2022-12-17 22:50:06 +0200

    mohamed hassan

--- app/src/main/java/com/example/hospital/doctor_details.java
+++ app/src/main/java/com/example/hospital/doctor_details.java
@@ -9,7 +9,7 @@ import android.widget.TextView;
 
 public class doctor_details extends AppCompatActivity {
 
-    TextView doctorName;
+    TextView  doctorName;
     TextView doctorSpec;
     TextView doctorPrice;
 
@@ -17,14 +17,14 @@ public class doctor_details extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_details);
+        int index = getIntent().getIntExtra("index" , 0);
 
-       int index = getIntent().getIntExtra("index",0);
 
         doctorName = findViewById(R.id.doc_name);
         doctorSpec = findViewById(R.id.dDoc_spec);
         doctorPrice = findViewById(R.id.dDoc_price);
-        //
-        doctorName.setText(provider.doctors.get(index).getFirstName()+""+provider.doctors.get(index).getLastName());
+
+        doctorName.setText(provider.doctors.get(index).getFirstName() + " " + provider.doctors.get(index).getLastName());
         doctorSpec.setText((provider.doctors.get(index).getSpecialty()));
         doctorPrice.setText(provider.doctors.get(index).getPrice());
 
