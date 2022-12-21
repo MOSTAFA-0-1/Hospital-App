a09345fb9410de15d3d223cea9a23d90afacf244
mohamedhassanabdullah <mh5418067@gmail.com>
2022-12-21 21:10:38 +0200

    mohamed hassan

--- app/src/main/java/com/example/hospital/Patien_Detailes_2.java
+++ app/src/main/java/com/example/hospital/Patien_Detailes_2.java
@@ -1,35 +1,44 @@
 package com.example.hospital;
 
+import static com.example.hospital.R.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class Patien_Detailes_2 extends AppCompatActivity {
 
 EditText Name;
-EditText age;
+//EditText age;
 EditText number;
 RadioButton male;
 RadioButton female;
 Button Continue;
+TextInputLayout AGE;
 boolean isMale = true;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_patien_detailes2);
+        setContentView(layout.activity_patien_detailes2);
 
 
 
-            Name = findViewById(R.id.editName22);
-            number = findViewById(R.id.editTextphNumber);
-            age = findViewById(R.id.editTextAge);
+            Name = findViewById(id.editName22);
+            number = findViewById(id.editTextphNumber);
+            //age = findViewById(id.editTextAge);
+            AGE = (TextInputLayout) findViewById(id.editTextAge1);
+            male = findViewById(id.RB_male);
+            female = findViewById(id.RB_female);
 
-            male = findViewById(R.id.RB_male);
-            female = findViewById(R.id.RB_female);
+
 
             male.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -37,23 +46,25 @@ boolean isMale = true;
                     isMale = true;
                 }
             });
-        female.setOnClickListener(new View.OnClickListener() {
+            female.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 isMale = false;
             }
         });
-            Continue = findViewById(R.id.debutton);
+            Continue = findViewById(id.debutton);
             Continue.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    if (!Name.getText().toString().isEmpty() && !age.getText().toString().isEmpty()
+                    if (!Name.getText().toString().isEmpty() && !AGE.getEditText().toString().isEmpty()
                             && !number.getText().toString().isEmpty()
                     )
-                    provider.setUserData(Name.getText().toString(),age.getText().toString(),isMale,number.getText().toString());
+                    {provider.setUserData(Name.getText().toString(),AGE.getEditText().toString(),isMale,number.getText().toString());}
                provider.intentTo(getApplicationContext(),MainActivity.class);
                 }
             });
+
+
 
     }
 
